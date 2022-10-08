3806c049aa325e72ed713a7edd9039814efcae9e
AI4817 <115336553+AI4817@users.noreply.github.com>
2022-10-08 14:07:32 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'wx8ae6dbb0bdbe5109',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '39a94412fb5b3b63574ae3b32ee8b890',
+      useTemplateId: '	V_6P5M0mEoHHeI6DD1AQf8a2Ux8ItYmkDW5qQlHi-wo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-17',
       festivals: [
